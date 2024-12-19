2cecec8745654e251592b883b4f99c4128e55581
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-12-19 12:57:23 -0500

    v1.55 - fully refactored to MVVM and working fully.

---
+++ BigPoly/Helpers/GradientMapView.swift
@@ -0,0 +1,10 @@
+//   GradientMapView.swift
+//   BigPoly
+//
+//   Created by: Gp. on 12/19/24 at 11:27 AM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import Foundation
---
+++ BigPoly/ViewModel/PolyViewModel.swift
@@ -0,0 +1,10 @@
+//   PolyViewModel.swift
+//   BigPoly
+//
+//   Created by: Gp. on 12/18/24 at 4:44 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import Foundation
